f639c7b4a8945886a02196adf32e5a43a0112c30
21gunni <gmh11@hi.is>
2018-02-14 20:03:02 +0000

    Vinnsla á search síu

--- app/src/main/java/is/hi/recipeapp/hugbv2/SearchActivity.java
+++ app/src/main/java/is/hi/recipeapp/hugbv2/SearchActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,14 +15,16 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.util.Log;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import static android.R.layout.simple_list_item_2;
 
 public class SearchActivity extends AppCompatActivity {
 
-    // private CheckBox checkBoxCicken, checkBoxBeef, checkBoxfish;
+    private CheckBox checkBoxCheese, checkBoxHam, checkBoxPasta;
     private Button showRecipe;
     private ListView listView;
-    ArrayList<String> listItems;
     ArrayAdapter<String> adapter;
     EditText editText;
 
@@ -32,10 +35,11 @@ public class SearchActivity extends AppCompatActivity {
 
 
     // Mock ingredients variables
-    private String[] ingredientsGrillCheese = {"Cheese", "Ham"};
-    private String[] ingredientsHotDog = {"Ketchup", "Fried Onion", "Mustard"};
+    private String[] ingredientsGrillCheese = {"Cheese", "Ham", "Bread"};
+    private String[] ingredientsHotDog = {"Ketchup", "Fried Onion", "Mustard", "Hot Dog", "Bread"};
     private String[] ingredientsPasta = {"Pasta", "Pepperoni", "Ham", "Carrots", "Bacon"};
     private String[] ingredientsCocoPuffs = {"Coco Puffs", "Milk"};
+
 
     // ArrayLists
     ArrayList<recipeSearchMock> allRecipies = new ArrayList<>();
@@ -44,15 +48,58 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
       
-        /*
-        checkBoxCicken = (CheckBox) findViewById(R.id.checkBoxChicken);
-        checkBoxBeef = (CheckBox) findViewById(R.id.checkBoxBeef);
-        checkBoxfish = (CheckBox) findViewById(R.id.checkBoxfish);
-        */
+
+        checkBoxCheese = (CheckBox) findViewById(R.id.checkBoxCheese);
+        checkBoxHam = (CheckBox) findViewById(R.id.checkBoxHam);
+        checkBoxPasta = (CheckBox) findViewById(R.id.checkBoxPasta);
 
 
         gestureObject = new GestureDetectorCompat(this, new LearnGesture());
+
+        showRecipe = findViewById(R.id.showRecipe);
+        listView = (ListView) findViewById(R.id.listView);
+
+        recipeSearchMock GrillCheese = new recipeSearchMock("Grill Cheese", ingredientsGrillCheese);
+        recipeSearchMock HotDog = new recipeSearchMock("Hot Dog", ingredientsHotDog);
+        recipeSearchMock Pasta = new recipeSearchMock("Pasta", ingredientsPasta);
+        recipeSearchMock CocoPuffs = new recipeSearchMock("Coco Puffs", ingredientsCocoPuffs);
+
+        allRecipies.add(GrillCheese);
+        allRecipies.add(HotDog);
+        allRecipies.add(Pasta);
+        allRecipies.add(CocoPuffs);
+
+
+        ArrayList<String> temp = new ArrayList<>();
+        ArrayList<String> tempIngred = new ArrayList<>();
+
+        for ( recipeSearchMock recipe : allRecipies ) {
+            Collections.addAll(tempIngred, recipe.getIngredients());
+            if(checkBoxPasta.isSelected()) {
+                
+            }
+        }
+
+        /*
+        for ( recipeSearchMock recipe : allRecipies ) {
+            temp.add(recipe.getName());
+        }
+         */
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, temp);
+
+
+        // Listener to view all recipies
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listView.setAdapter(adapter);
+            }
+        };
+        showRecipe.setOnClickListener(listener);
     }
 
     @Override
@@ -80,41 +127,6 @@ public class SearchActivity extends AppCompatActivity {
             }
             return true;
         }
-
-/*
-        showRecipe = findViewById(R.id.showRecipe);
-        listView = (ListView) findViewById(R.id.listView);
-        listItems = new ArrayList<String>();
-
-        listItems.add("First Item - added on Activity Create");
-        listItems.add("Second Item - added on Activity Create");
-        listItems.add("Third Item - added on Activity Create");
-
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, listItems);
-
-        //listView.setAdapter(adapter);
-
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                listView.setAdapter(adapter);
-            }
-        };
-        showRecipe.setOnClickListener(listener);
-
-        recipeSearchMock GrillCheese = new recipeSearchMock("Grill Cheese", ingredientsGrillCheese);
-        recipeSearchMock HotDog = new recipeSearchMock("Hot Dog", ingredientsHotDog);
-        recipeSearchMock Pasta = new recipeSearchMock("Pasta", ingredientsPasta);
-        recipeSearchMock CocoPuffs = new recipeSearchMock("Coco Puffs", ingredientsCocoPuffs);
-
-        allRecipies.add(GrillCheese);
-        allRecipies.add(HotDog);
-        allRecipies.add(Pasta);
-        allRecipies.add(CocoPuffs);
-*/
-
     }
 }
 
