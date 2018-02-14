45e84420d90dd9ffd2120179894cef0ceb1c3592
PeturLogi <plp2@hi.is>
2018-02-14 06:06:10 +0000

    Lagaði síðasta commit

---
+++ app/src/main/java/is/hi/recipeapp/hugbv2/SignupActivity.java
@@ -0,0 +1,194 @@
+package is.hi.recipeapp.hugbv2;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    @BindView(R.id.input_name) EditText _nameText;
+    @BindView(R.id.input_address) EditText _addressText;
+    @BindView(R.id.input_email) EditText _emailText;
+    @BindView(R.id.input_mobile) EditText _mobileText;
+    @BindView(R.id.input_password) EditText _passwordText;
+    @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+    @BindView(R.id.btn_signup) Button _signupButton;
+    @BindView(R.id.link_login) TextView _loginLink;
+
+    private static Account newAccount;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connection to database
+     */
+    private static final ArrayList<String> DUMMY_CREDENTIALS = new ArrayList<String>();
+    private String[] temp = new String[] {
+            "foo@example.com:hello", "bar@example.com:world", "hopur6@hi.is:hopur6",
+            "testmail@gmail.com:TestPassWord123"
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        for (int i = 0; i < temp.length; i++) {
+            DUMMY_CREDENTIALS.add(temp[i]);
+        }
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        // Signup logic:
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        boolean token = true;
+                        // Checks if email and password already exists
+                        for (String credential : DUMMY_CREDENTIALS) {
+                            String[] pieces = credential.split(":");
+                            if(pieces[0].equals(_emailText.getText().toString())) {
+                                token = false;
+                            }
+                        }
+                        if(token) {
+                            onSignupSuccess();
+                        } else {
+                            onSignupFailed();
+                        }
+
+                        //onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        newAccount = new Account(name, address, email, mobile, password);
+
+        //TODO: replace after connection to database
+        DUMMY_CREDENTIALS.add(name + ":" + password);
+        Toast.makeText(getBaseContext(), "Signup successful!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(SignupActivity.this, SearchActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Signup failed, email already in use", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (address.isEmpty()) {
+            _addressText.setError("Enter Valid Address");
+            valid = false;
+        } else {
+            _addressText.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length()!=7) {
+            _mobileText.setError("Enter Valid Mobile Number");
+            valid = false;
+        } else {
+            _mobileText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("Password Do not match");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+}
